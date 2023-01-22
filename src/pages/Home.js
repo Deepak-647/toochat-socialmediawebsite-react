314cecd133d7008ff1c05ed56e7c693e643f08cc
Deepak <dbehera647@gmail.com>
2023-01-22 11:55:43 +0530

    Styling our Homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,37 +1,52 @@
+import styles from '../styles/home.module.css';
+
 const Home = () => {
   return (
-    <div className="posts-list">
-      <div className="post-wrapper">
-        <div className="post-header">
-          <div className="post-avatar">
-            <img src="" alt="user-pic" />
+    <div className={styles.postsList}>
+      <div className={styles.postWrapper}>
+        <div className={styles.postHeader}>
+          <div className={styles.postAvatar}>
+            <img
+              src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
+              alt="user-pic"
+            />
             <div>
-              <span className="post-author">Deepak </span>
-              <span className="post-time">a minute ago</span>
+              <span className={styles.postAuthor}>Deepak</span>
+              <span className={styles.postTime}>a minute ago</span>
             </div>
           </div>
-          <div className="post-content">Post Content</div>
-          <div className="post-actions">
-            <div className="post-like">
-              <img src=" " alt="likes-icon" />
+          <div className={styles.postContent}>Post Conetnt</div>
+
+          <div className={styles.postActions}>
+            <div className={styles.postLike}>
+              <img
+                src="https://image.flaticon.com/icons/svg/1077/1077035.svg"
+                alt="likes-icon"
+              />
               <span>5</span>
             </div>
-            <div className="post-comments-icon">
-              <img src="" alt="comments-icon" />
+
+            <div className={styles.postCommentsIcon}>
+              <img
+                src="https://image.flaticon.com/icons/svg/1380/1380338.svg"
+                alt="comments-icon"
+              />
               <span>2</span>
             </div>
           </div>
-          <div className="post-comment-box">
-            <input placeholder="Start typing a comment .."/>
+          <div className={styles.postCommentBox}>
+            <input placeholder="Start typing a comment" />
           </div>
-          <div className="post-comments-list">
-            <div className="post-commnets-item">
-              <div className="post-comments-header">
-                <span className="post-comment-author">Duke </span>
-                <span className="post-comment-time">a minute ago</span>
-                <span className="post-comment-likes">22</span>
+
+          <div className={styles.postCommentsList}>
+            <div className={styles.postCommentsItem}>
+              <div className={styles.postCommentHeader}>
+                <span className={styles.postCommentAuthor}>Bill</span>
+                <span className={styles.postCommentTime}>a minute ago</span>
+                <span className={styles.postCommentLikes}>22</span>
               </div>
-              <div className="post-comment-content">Random Comment</div>
+
+              <div className={styles.postCommentContent}>Random comment</div>
             </div>
           </div>
         </div>
@@ -39,4 +54,5 @@ const Home = () => {
     </div>
   );
 };
+
 export default Home;
