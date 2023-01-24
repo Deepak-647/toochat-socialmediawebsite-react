4397264f918be20c0842ef2d01a4e86f7fa1c7ee
Deepak <dbehera647@gmail.com>
2023-01-24 10:56:30 +0530

    Added Routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,22 @@
 import { useEffect, useState } from 'react';
-
+import { Routes , Route } from 'react-router-dom';
 import { getPosts } from '../api';
-import { Home } from '../pages';
+import { Home , Login } from '../pages';
 import { Loader } from './';
 import  Navbar from './Navbar';
+
+const About = () => {
+  return <h1>About</h1>;
+};
+
+const UserInfo = () => {
+  return <h1>User</h1>;
+};
+
+const Page404 = () => {
+  return <h1>404</h1>;
+};
+
 
 function App() {
   const [posts, setPosts] = useState([]);
@@ -30,7 +43,27 @@ function App() {
   return (
     <div className="App">
       <Navbar/>
-      <Home posts={posts} />
+      <Routes>
+        <Route  path="/" element={<Home posts={posts} />}/>
+          
+        
+
+        <Route  path="/login" element={ <Login />}/>
+         
+        
+
+        <Route  path="/about" element={<About />}/>
+          
+        
+
+        <Route  path="/user/asdasd" element={   <UserInfo />}/>
+       
+        
+
+        <Route path="*" element={<Page404 />}/>
+          
+        
+      </Routes>
     </div>
   );
 }
