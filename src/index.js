532a1775a0cff80fc6117c394e498ad3e89daeee
Deepak <dbehera647@gmail.com>
2023-01-24 16:29:08 +0530

    used toast-notifications to show notification and used hooks in our login form

--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ import './styles/index.css';
 // import App from './App';
 import {App} from './components';
 import { BrowserRouter as Router } from 'react-router-dom';
- 
+import { ToastProvider } from 'react-toast-notifications';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ToastProvider autoDismiss autoDismissTimeout={5000} placement='top-left'>
     <Router>
     <App />
     </Router>
+    </ToastProvider>
   </React.StrictMode>
 );
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,22 +1,48 @@
+import { useState } from 'react';
+import { useToasts } from 'react-toast-notifications';
 import styles from '../styles/login.module.css';
-const Login = () => {
+
+const Login = () =>{
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loggingIn, setLoggingIn] = useState(false);
+  const { addToast } = useToasts();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoggingIn(true);
+
+    if (!email || !password) {
+      return addToast('Please enter both email and password', {
+        appearance: 'error',
+
+      });
+    }
+  };
+
    return (
-    <form className={styles.loginForm}>
+    <form className={styles.loginForm} onSubmit={handleSubmit}>
       <span className={styles.loginSignupHeader}>Log In</span>
 
       <div className={styles.field}>
-        <input type="email" placeholder="Email" required />
+        <input type="email" placeholder="Email"
+          vaue={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </div>
 
       <div className={styles.field}>
-        <input type="password" placeholder="Paasword" required />
+        <input type="password" placeholder="Paasword" 
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </div>
 
-      <div className={styles.field}>
-        <button>Log In</button>
+      <div className={styles.field} disabled={loggingIn}>
+        <button>{loggingIn ? "Logging in .." : "Log in " }</button>
       </div>
     </form>
   );
 };
 
-export default Login;
+export default Login ;
