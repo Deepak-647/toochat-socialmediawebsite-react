363b531454ec421d16179b8db045ea4e7dd3a379
Deepak <dbehera647@gmail.com>
2023-01-30 08:51:24 +0530

    Created getFormBody function to fetch the body

--- src/utils/index.js
+++ src/utils/index.js
@@ -9,3 +9,16 @@
 //doing all the stuffs in a single line like this
 
 export * from './constants';
+
+export const getFormBody = (params) => {
+    let formBody = [];
+  
+    for (let property in params) {
+      let encodedKey = encodeURIComponent(property); // 'user name' => 'user%20name'
+      let encodedValue = encodeURIComponent(params[property]); // aakash 123 => aakash%2020123
+  
+      formBody.push(encodedKey + '=' + encodedValue);
+    }
+  
+    return formBody.join('&'); // 'username=aakash&password=123213'
+  };
