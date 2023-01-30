a163dff852660bd4a6cffd9661f319fc485ac437
Deepak <dbehera647@gmail.com>
2023-01-30 09:31:10 +0530

    created login logout in useProvideAuth hooks

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,16 @@
 import { useState } from 'react';
 import { useToasts } from 'react-toast-notifications';
-import styles from '../styles/login.module.css';
-import {login} from '../api'
 
-const Login = () =>{
+import styles from '../styles/login.module.css';
+import { useAuth } from '../hooks';
+
+const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [loggingIn, setLoggingIn] = useState(false);
   const { addToast } = useToasts();
+  const auth = useAuth();
+  console.log(auth);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -16,10 +19,10 @@ const Login = () =>{
     if (!email || !password) {
       return addToast('Please enter both email and password', {
         appearance: 'error',
-
       });
     }
-    const response = await login(email, password);
+
+    const response = await auth.login(email, password);
 
     if (response.success) {
       addToast('Successfully logged in', {
@@ -30,32 +33,39 @@ const Login = () =>{
         appearance: 'error',
       });
     }
+
     setLoggingIn(false);
   };
 
-   return (
+  return (
     <form className={styles.loginForm} onSubmit={handleSubmit}>
       <span className={styles.loginSignupHeader}>Log In</span>
 
       <div className={styles.field}>
-        <input type="email" placeholder="Email"
-          vaue={email}
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
       </div>
 
       <div className={styles.field}>
-        <input type="password" placeholder="Paasword" 
+        <input
+          type="password"
+          placeholder="Paasword"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
       </div>
 
-      <div className={styles.field} >
-        <button disabled={loggingIn}>{loggingIn ? "Logging in .." : "Log in " }</button>
+      <div className={styles.field}>
+        <button disabled={loggingIn}>
+          {loggingIn ? 'Logging in...' : 'Log In'}
+        </button>
       </div>
     </form>
   );
 };
 
-export default Login ;
+export default Login;
