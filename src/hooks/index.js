cd7c693a36a01757e6514382ba1dc5db78c886eb
Deepak <dbehera647@gmail.com>
2023-01-30 15:07:03 +0530

    Persisting  user

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,6 +1,7 @@
 import { useContext, useState } from "react";
 import {AuthContext} from '../providers/AuthProvider';
 import {login as userLogin} from '../api'
+import { setItemInLocalStorage , removeItemFromLocalStorage, LOCALSTORAGE_TOKEN_KEY } from "../utils";
 
 export const useAuth =() => {
   return useContext(AuthContext)
@@ -14,6 +15,10 @@ export const useProvideAuth = () => {
 
     if (response.success) {
       setUser(response.data.user);
+      setItemInLocalStorage(
+        LOCALSTORAGE_TOKEN_KEY,
+        response.data.token ? response.data.token : null
+      );
       return {
         success: true,
       };
@@ -27,6 +32,7 @@ export const useProvideAuth = () => {
 
   const logout = () => {
     setUser(null);
+    removeItemFromLocalStorage(LOCALSTORAGE_TOKEN_KEY);
   };
 
   return {
