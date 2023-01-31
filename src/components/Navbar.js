9ec02e2f838e58b4c76ca8e4566ae04f182480b3
Deepak <dbehera647@gmail.com>
2023-01-31 11:07:53 +0530

    useAuth in Navabar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -44,3 +44,64 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+
+//THINGS NEED TO BE CHANGE WHILE USING THE useAuth () hook
+
+// import { Link } from 'react-router-dom';
+
+// import styles from '../styles/navbar.module.css';
+// import { useAuth } from '../hooks';
+
+// const Navbar = () => {
+//   const auth = useAuth();
+
+//   return (
+//     <div className={styles.nav}>
+//       <div className={styles.leftDiv}>
+//         <Link to="/">
+//           <img
+//             alt=""
+//             src="https://ninjasfiles.s3.amazonaws.com/0000000000003454.png"
+//           />
+//         </Link>
+//       </div>
+
+//       <div className={styles.rightNav}>
+//         {auth.user && (
+//           <div className={styles.user}>
+//             <a href="/">
+//               <img
+//                 src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
+//                 alt=""
+//                 className={styles.userDp}
+//               />
+//             </a>
+//             <span>{auth.user.name}</span>
+//           </div>
+//         )}
+
+//         <div className={styles.navLinks}>
+//           <ul>
+//             {auth.user ? (
+//               <>
+//                 <li onClick={auth.logout}>Log out</li>
+//               </>
+//             ) : (
+//               <>
+//                 <li>
+//                   <Link to="/login">Log in</Link>
+//                 </li>
+//                 <li>
+//                   <a href="/">Register</a>
+//                 </li>
+//               </>
+//             )}
+//           </ul>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Navbar;
