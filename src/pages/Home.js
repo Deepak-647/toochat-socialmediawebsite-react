87a5a51e78cf26a8632d22bff7735a3e43bb5c29
Deepak <dbehera647@gmail.com>
2023-01-31 10:45:06 +0530

    used the useEffect in Home component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,30 @@
+import { useEffect, useState } from 'react';
+
+import { Comment, Loader } from '../components';
+import { getPosts } from '../api';
 import styles from '../styles/home.module.css';
 
 const Home = () => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await getPosts();
+
+      if (response.success) {
+        setPosts(response.data.posts);
+      }
+
+      setLoading(false);
+    };
+
+    fetchPosts();
+  }, []);
+
+  if (loading) {
+    return <Loader />;
+  }
   return (
     <div className={styles.postsList}>
       <div className={styles.postWrapper}>
