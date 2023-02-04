60e8421c1c829d6a1a9d2914e62b198a9abb66ef
Deepak <dbehera647@gmail.com>
2023-02-04 16:02:09 +0530

    Improving UserProfile logic

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -1,14 +1,53 @@
-import { useState } from 'react';
+import { useEffect,useState } from 'react';
 import { useToasts } from 'react-toast-notifications';
-import { useLocation } from 'react-router-dom';
+import { Loader } from '../components';
+import { useParams ,useNavigate } from 'react-router-dom';
 import styles from '../styles/settings.module.css';
 import { useAuth } from '../hooks';
+import { fetchUserProfile } from '../api';
 
 const UserProfile = () => {
-  const location = useLocation();
-  console.log('location', location);
-  const { user = {} } = location.state;
+  const [user, setUser] = useState({});
+  const [loading, setLoading] = useState(true);
+  const { userId } = useParams();
+  const { addToast } = useToasts();
+  const history = useNavigate();
+  const auth = useAuth();
 
+  useEffect(() => {
+    const getUser = async () => {
+      const response = await fetchUserProfile(userId);
+
+      if (response.success) {
+        setUser(response.data.user);
+      } else {
+        addToast(response.message, {
+          appearance: 'error',
+        });
+        return history.push('/');
+      }
+
+      setLoading(false);
+    };
+
+    getUser();
+  }, [userId, history, addToast]);
+
+  if (loading) {
+    return <Loader />;
+  }
+  const checkIfUserIsAFriend = () => {
+    const friends = auth.user.friends;
+
+    const friendIds = friends.map((friend) => friend.to_user._id);
+    const index = friendIds.indexOf(userId);
+
+    if (index !== -1) {
+      return true;
+    }
+
+    return false;
+  };
   return (
     <div className={styles.settings}>
       <div className={styles.imgContainer}>
@@ -30,9 +69,12 @@ const UserProfile = () => {
       </div>
 
       <div className={styles.btnGrp}>
-        <button className={`button ${styles.saveBtn}`}>Add friend</button>
-
-        <button className={`button ${styles.saveBtn}`}>Remove friend</button>
+      {checkIfUserIsAFriend() ? (
+          <button className={`button ${styles.saveBtn}`}>Remove friend</button>
+        ) : (
+          <button className={`button ${styles.saveBtn}`}>Add friend</button>
+        )}
+       
       </div>
     </div>
   );
