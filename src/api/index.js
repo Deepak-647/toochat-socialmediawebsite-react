6d4e344843ff4f5e4e8198363bd67fb3e3658342
Deepak <dbehera647@gmail.com>
2023-02-07 13:08:46 +0530

    Creating CreatePost logic

--- src/api/index.js
+++ src/api/index.js
@@ -92,3 +92,12 @@ export const removeFriend = (userId) => {
     method: 'POST',
   });
 };
+
+export const addPost = (content) => {
+  return customFetch(API_URLS.createPost(), {
+    method: 'POST',
+    body: {
+      content,
+    },
+  });
+};
