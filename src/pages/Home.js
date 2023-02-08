a3aba880161f7e8e505651e9ea304fc0c825ab25
Deepak <dbehera647@gmail.com>
2023-02-08 12:28:38 +0530

    Adding the Comment section and the functionality

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Comment, Loader ,FriendsList ,CreatePost} from '../components';
+import { Comment, Loader ,FriendsList ,CreatePost,Post} from '../components';
 import { getPosts } from '../api';
 import styles from '../styles/home.module.css';
 import { useAuth, usePosts  } from '../hooks';
@@ -93,61 +93,30 @@ export default Home;
 
 //THINGS NEED TO BE CHANGED WHILE DISPALYING POSTS FROM API ... For now our Api is not working that's why we commented this
 
+// import { Post, Loader, FriendsList, CreatePost } from '../components';
 // import styles from '../styles/home.module.css';
-// import PropTypes from 'prop-types';
-// import { Comment } from '../components';
+// import { useAuth, usePosts } from '../hooks';
 
-// const Home = ({ posts }) => {
+// const Home = () => {
+//   const auth = useAuth();
+//   const posts = usePosts();
+
+//   if (posts.loading) {
+//     return <Loader />;
+//   }
+
 //   return (
-//     <div className={styles.postsList}>
-//       {posts.data.map((post) => (
-//         <div className={styles.postWrapper} key={`post-${post._id}`}>
-//           <div className={styles.postHeader}>
-//             <div className={styles.postAvatar}>
-//               <img
-//                 src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
-//                 alt="user-pic"
-//               />
-//               <div>
-//                 <span className={styles.postAuthor}>{post.user.name}</span>
-//                 <span className={styles.postTime}>a minute ago</span>
-//               </div>
-//             </div>
-//             <div className={styles.postContent}>{post.conent}</div>
-
-//             <div className={styles.postActions}>
-//               <div className={styles.postLike}>
-//                 <img
-//                   src="https://image.flaticon.com/icons/svg/1077/1077035.svg"
-//                   alt="likes-icon"
-//                 />
-//                 <span>5</span>
-//               </div>
-
-//               <div className={styles.postCommentsIcon}>
-//                 <img
-//                   src="https://image.flaticon.com/icons/svg/1380/1380338.svg"
-//                   alt="comments-icon"
-//                 />
-//                 <span>2</span>
-//               </div>
-//             </div>
-//             <div className={styles.postCommentBox}>
-//               <input placeholder="Start typing a comment" />
-//             </div>
-
-//             <div className={styles.postCommentsList}>
-//                    {post.comments.map((comment) => (
-//                     <Comment comment={comment} />
-//                     ))}
-//             </div>
-//           </div>
-//         </div>
-//       ))}
+//     <div className={styles.home}>
+//       <div className={styles.postsList}>
+//         <CreatePost />
+//         {posts.data.map((post) => (
+//           <Post post={post} key={`post-${post._id}`} />
+//         ))}
+//       </div>
+//       {auth.user && <FriendsList />}
 //     </div>
 //   );
 // };
-// Home.propTypes = {
-//   posts: PropTypes.array.isRequired,
-// };
+
 // export default Home;
+
